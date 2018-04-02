e08ca53da8b9f2c1aae69b6087450431d774012b
Kyle Harris <k_harris95@live.com>
2018-04-02 04:32:54 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,4 +5,17 @@ class Doctor
     @name = name
     @appointments = []
   end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def patients
+    @appointments.collect { |appointment| appointment.patient }
+  end
 end
